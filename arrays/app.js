66cab4d6e60cc6d1d994abc6299a548285507f6b
royliangsehhaw <liangsehhaw@gmail.com>
2022-05-27 22:04:27 +0800

    Changes

---
+++ arrays/app.js
@@ -0,0 +1,20 @@
+var intArray = [23,44,66,1,12,56,13,19];
+var stringArray = ['aaa', 'ccc', 'ddd', 'eee', 'kkk', 'ppp']
+var objectArray = [
+    {id: 1, race: 'terran',  unit: 'marine',  weapon: 3, armor: 1},
+    {id: 2, race: 'protoss', unit: 'stalker', weapon: 2, armor: 1},
+    {id: 3, race: 'terran',  unit: 'viking',  weapon: 2, armor: 3},
+    {id: 4, race: 'terran',  unit: 'banshee', weapon: 3, armor: 1},
+]
+
+// --- 1
+
+// --- 2
+
+// --- 3
+
+// --- 4
+
+// --- 5
+
+alert('hello');
